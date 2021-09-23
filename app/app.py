65d12261fd45abfe6455993b01cba9f005f10a90
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-09-23 18:01:26 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from flask import Flask, make_response, request, current_app
+from flask import Flask, make_response, request, current_app, jsonify
 from functools import update_wrapper
 
 import requests
@@ -11,63 +11,22 @@ import json
 
 app = Flask(__name__)
 
-# crossdomain decorator
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not headers:
-        headers = ', '.join(x.upper() for x in headers)
-    if not origin:
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
 # endpoints
 @app.route('/')
-@crossdomain(origin='*')
 def home():
     return { 'status': 'ok', 'more_at': 'https://scratchhh.tk' }
 
 @app.route('/users/<user>/')
-@crossdomain(origin='*')
 def user(user):
-    return json.loads(requests.get(f'https://api.scratch.mit.edu/users/{user}').text)
+    res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/users/'+user).text)).headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 @app.route('/projects/<id>/')
-@crossdomain(origin='*')
 def project(id):
-    return json.loads(requests.get(f'https://api.scratch.mit.edu/projects/{id}').text)
+    res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/projects/'+id).text)).headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 @app.route('/featured/')
-@crossdomain(origin='*')
 def featured():
-    return json.loads(requests.get('https://api.scratch.mit.edu/proxy/featured').text)
+    res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/proxy/featured').text)).headers.add('Access-Control-Allow-Origin', '*')
+    return res
