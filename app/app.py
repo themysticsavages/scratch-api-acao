f3bcfff43283a1366c6ce67373bd6fdff9d937cd
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-09-23 18:09:02 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -19,14 +19,17 @@ def home():
 @app.route('/users/<user>/')
 def user(user):
     res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/users/'+user).text))
+    res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 @app.route('/projects/<id>/')
 def project(id):
     res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/projects/'+id).text))
+    res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 @app.route('/featured/')
 def featured():
     res = jsonify(json.loads(requests.get('https://api.scratch.mit.edu/proxy/featured').text))
+    res.headers.add('Access-Control-Allow-Origin', '*')
     return res
