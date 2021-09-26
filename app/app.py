316e278c295cdab3a033ce365416da80a157feeb
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-09-26 07:06:57 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -49,4 +49,32 @@ def checkuser():
     res = jsonify(res)
     res.headers.add('Access-Control-Allow-Origin', '*')
     return res
+
+@app.get('/api/fetchbackpack/')
+def fetchback():
+    args = request.args
+    def get_user_backpack():
+            sess = scratchclient.ScratchSession(args.get('user'), args.get('pass'))
+            headers = {
+                  "x-csrftoken": sess.csrf_token,
+                  "X-Token": sess.token,
+                  "x-requested-with": "XMLHttpRequest",
+                  "Cookie": "scratchcsrftoken="
+                  + sess.csrf_token
+                  + ";scratchlanguage=en;scratchsessionsid="
+                  + sess.session_id
+                  + ";",
+                  "referer": "https://scratch.mit.edu/users/" + args.get('user') + "/",
+            }
+            req = requests.get('https://backpack.scratch.mit.edu/'+args.get('user')+'/', headers=headers)
+            return req.text
+    userb = json.loads(get_user_backpack())
+
+    content = []
+      for cnt in userb:
+            content.append(cnt['body'])
+    res = jsonify(eval(str(content)))
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    print(res.headers)
+    return res
     
